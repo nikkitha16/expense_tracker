3d1daffa41657a30f5c06a28be5f2a277ff707f8
Nikkitha Ravi <nikkitharavi2001@gmail.com>
2025-07-03 14:04:27 +0530

    added few changes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use(cors({
 }));
 app.use(express.json());
 connectDB();
-
+app.get("/", (req, res) => {
+  res.send("Expense Tracker API is running ✅");
+});
 app.use("/api/v1/auth",authRoutes);
 app.use("/api/v1/income",incomeRoutes);
 app.use("/api/v1/expense",expenseRoutes); 
--- frontend/expense-tracker/src/utils/apiPath.js
+++ frontend/expense-tracker/src/utils/apiPath.js
@@ -1,4 +1,4 @@
-export const BASE_URL="http://localhost:8000";
+export const BASE_URL="https://expense-tracker-backend-vzud.onrender.com/";
 
 export const API_PATHS={
     AUTH:{
