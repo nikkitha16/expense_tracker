6db561cb85ca19ee47dc3d23ae5edc5269b30964
Nikkitha Ravi <nikkitharavi2001@gmail.com>
2025-07-03 19:42:17 +0530

    removed error in render

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
     methods:["GET", "POST", "PUT", "DELETE"],
     allowedHeaders:["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+// app.options("*", cors());
 
 app.use(express.json());
 connectDB();
